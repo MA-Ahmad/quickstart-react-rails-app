46ba762b812d6c9d2f4581f32022aef6663af3df
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-19 14:23:29 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Main from './components/Main';
 
-function App() {
+import { AuthProvider } from './contexts/auth';
+import { UserProvider } from './contexts/user';
+
+function App(props) {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <AuthProvider>
+      <UserProvider>
+        <ChakraProvider theme={theme}>
+          <Box textAlign="center" fontSize="xl">
+            {/* <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        </Grid> */}
+            <Main {...props} />
+          </Box>
+        </ChakraProvider>
+      </UserProvider>
+    </AuthProvider>
   );
 }
 
