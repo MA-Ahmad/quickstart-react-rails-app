a6420823eb9a9ca89f4ab731f00c2b7587797555
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-19 14:23:21 -0700

    Create authentication.js

---
+++ src/apis/authentication.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/';
+
+const login = payload => axios.post(`${baseUrl}api/v1/login`, payload);
+
+const logout = () => axios.delete(`${baseUrl}api/v1/logout`);
+
+const signup = payload => axios.post(`${baseUrl}api/v1/users`, payload);
+
+const authenticationApi = {
+  login,
+  logout,
+  signup,
+};
+
+export default authenticationApi;
