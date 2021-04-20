4101820b818f28d8ceb5800263bcbe69c5343309
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-20 01:57:33 -0700

    Update authentication.js

--- src/apis/authentication.js
+++ src/apis/authentication.js
@@ -5,11 +5,9 @@ const baseUrl = 'https://auth-app-rails-back.herokuapp.com/';
 
 const login = payload => axios.post(`${baseUrl}api/v1/login`, payload);
 
-const logout = () =>
-  axios.delete(`${baseUrl}api/v1/logout`, { withCredentials: true });
+const logout = () => axios.delete(`${baseUrl}api/v1/logout`);
 
-const signup = payload =>
-  axios.post(`${baseUrl}api/v1/users`, payload, { withCredentials: true });
+const signup = payload => axios.post(`${baseUrl}api/v1/users`, payload);
 
 const authenticationApi = {
   login,
