574048d88fb0caf8673c8a4b4b31106552741384
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-20 01:30:35 -0700

    Update authentication.js

--- src/apis/authentication.js
+++ src/apis/authentication.js
@@ -3,11 +3,14 @@ import axios from 'axios';
 // const baseUrl = 'http://localhost:3001/';
 const baseUrl = 'https://auth-app-rails-back.herokuapp.com/';
 
-const login = payload => axios.post(`${baseUrl}api/v1/login`, payload);
+const login = payload =>
+  axios.post(`${baseUrl}api/v1/login`, payload, { withCredentials: true });
 
-const logout = () => axios.delete(`${baseUrl}api/v1/logout`);
+const logout = () =>
+  axios.delete(`${baseUrl}api/v1/logout`, { withCredentials: true });
 
-const signup = payload => axios.post(`${baseUrl}api/v1/users`, payload);
+const signup = payload =>
+  axios.post(`${baseUrl}api/v1/users`, payload, { withCredentials: true });
 
 const authenticationApi = {
   login,
