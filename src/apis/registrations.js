7e18dbc86ff0d1f2560c7b9d0edc724f4f171854
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-21 03:03:21 -0700

    updated baseUrl

--- src/apis/registrations.js
+++ src/apis/registrations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/';
-// const baseUrl = 'https://auth-app-rails-back.herokuapp.com/';
+// const baseUrl = 'http://localhost:3001/';
+const baseUrl = 'https://auth-app-rails-back.herokuapp.com/';
 
 const updatePassword = payload =>
   axios.put(`${baseUrl}api/v1/password/update`, payload);
