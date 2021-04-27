3342e45520167b023b09c085c8cb49c9cff80878
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-04-27 13:25:22 -0700

    height issue fixed

--- src/components/Dashboard/MainSection.jsx
+++ src/components/Dashboard/MainSection.jsx
@@ -10,7 +10,7 @@ import {
 
 export default function MainSection() {
   return (
-    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+    <Stack style={{ minHeight: 'calc(100vh - 4rem)' }} direction={{ base: 'column', md: 'row' }}>
       <Flex p={8} flex={1} align={'center'} justify={'center'}>
         <Stack spacing={6} w={'full'} maxW={'lg'}>
           <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
